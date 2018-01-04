0b8f3a13323ffef19050f1c3851e104165e0cec5
Daniel Micallef <danielmicallef94@gmail.com>
2018-01-04 17:56:14 +0100

    variable date range;
    Added get_company_price_changes() function

--- src/tip_ranks.py
+++ src/tip_ranks.py
@@ -28,7 +28,7 @@ def read_from_pickle(ticker):
         return None
 
 
-def company_data_gen(tickers, update=False):
+def company_data_gen(tickers, update=False, to_date= datetime.today(), date_range = 90):
     headers = {
         'User-Agent': 'Mozilla/5.0 (X11; Linux i686) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1312.27 Safari/537.17'
     }
@@ -49,7 +49,7 @@ def company_data_gen(tickers, update=False):
             print("Data loaded. Retrieving Expert Price Target")
 
             # retrieve only reviews within the last 90 days
-            target_date_range = datetime.today() - timedelta(days=90)
+            from_date = to_date - timedelta(days=date_range)
 
             # expert price target
             expert_price_targets = [
@@ -61,7 +61,8 @@ def company_data_gen(tickers, update=False):
                 }
                 for expert in data['experts']
                 for rating in expert['ratings']
-                if rating['priceTarget'] is not None and parse(rating['time']) > target_date_range
+                if rating['priceTarget'] is not None and
+                   from_date < parse(rating['time']) < to_date
             ]
 
             price_targets = [priceTarget['priceTarget'] for priceTarget in expert_price_targets]
@@ -88,6 +89,11 @@ def company_data_gen(tickers, update=False):
 # path = f"https://www.tipranks.com/api/stocks/getNewsSentiments/?ticker={ticker}"
 # path = f"https://www.tipranks.com/api/stocks/getNews/?ticker={ticker}"
 
+
+def get_company_price_changes(ticker, from_date, to_date):
+    import pandas_datareader.data as web
+    f = web.DataReader("F", 'google', from_date, to_date)
+
 def update_data(tickers):
     print("tickers retrieved.")
 
@@ -104,11 +110,18 @@ def retrieve_data(tickers = get_sp500_tickers(), update=False):
         if data is not None:
             yield data
 
-sp500_data = []
+def test_data(update = False):
+    sp500_data = []
 
-for company in retrieve_data():
-    sp500_data.append(company)
+    for company in retrieve_data(update= update):
+        sp500_data.append(company)
 
-sp500_data.sort(key=lambda k: k['averageExpectedPercChange'])
+    sp500_data.sort(key=lambda k: k['averageExpectedPercChange'])
 
 # update_data()
+
+    for company in sp500_data[:10]:
+       print("Company: " + company['ticker']+" Rel Std Dev: " + company['relStdDevPriceTargets'] + " Avg Expected Perc Change " + company['averageExpectedPercChange'])
+
+company_gen = company_data_gen(get_sp500_tickers())
+next(company_gen)
